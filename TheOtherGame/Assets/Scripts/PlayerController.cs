47117740d7c3d0964b1098cc34006113a314dad4
martin01034 <kicktv8@gmail.com>
2019-11-01 13:51:25 +0100

    Player replaced by character with simple walking animation, but it's still not perfect

--- TheOtherGame/Assets/Scripts/PlayerController.cs
+++ TheOtherGame/Assets/Scripts/PlayerController.cs
@@ -6,49 +6,62 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float speed = 15;
-    
+
+    private Animator animator;
+    bool going = false;
+
     // Start is called before the first frame update
 
     void Start()
     {
-
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        animator.SetBool("go", going);
     }
 
     void Movement()
     {
+        if (Input.GetKey("a") || Input.GetKey("s") || Input.GetKey("d") || Input.GetKey("w"))
+        {
+            Vector3 movement = new Vector3();
+            if (Input.GetKey("a"))
+            {
+                movement += new Vector3(-speed, 0);
+                going = true;
+            }
+            if (Input.GetKey("d"))
+            {
+                movement += new Vector3(speed, 0);
+                going = true;
+            }
+            if (Input.GetKey("s"))
+            {
+                movement += new Vector3(0, 0, -speed);
+            }
+            if (Input.GetKey("w"))
+            {
+                movement += new Vector3(0, 0, speed);
+            }
+            //if no key is pressed then do nothing
+            if (movement != Vector3.zero)
+            {
+                //otherwise set the rotation of the player and the position
+                this.transform.rotation = Quaternion.LookRotation(movement);
+                this.transform.Translate(movement * Time.deltaTime, Space.World);
+            }
+            going = true;
+        }
+        else
+            going = false;
 
-        Vector3 movement = new Vector3();
-        if (Input.GetKey("a"))
-        {                                                                                    
-            movement += new Vector3(-speed, 0);
-        }
-        if (Input.GetKey("d"))
-        {
-            movement += new Vector3(speed, 0);
-        }
-        if (Input.GetKey("s"))
-        {
-            movement += new Vector3(0, 0, -speed);
-        }
-        if (Input.GetKey("w"))
-        {
-            movement += new Vector3(0, 0, speed);
-        }
-        //if no key is pressed then do nothing
-        if (movement != Vector3.zero)
-        {
-            //otherwise set the rotation of the player and the position
-            this.transform.rotation = Quaternion.LookRotation(movement);
-            this.transform.Translate(movement * Time.deltaTime, Space.World);
-        }
 
-        
+
+
 
     }
 }
